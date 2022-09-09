d2efa6bc81ff86407fb89893814642a113bbe69a
temo-sudo <you@example.com>
2022-09-09 22:01:56 +0300

    Desigin Faourites list and implementing its functionality

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 let darkMode = JSON.parse(localStorage.getItem('mode'));
+let favourites = JSON.parse(localStorage.getItem('fav')) || [];
 
-
+let favCountries = [];
 let allCountries = [];
 let currCountries = [];
+let isFirstDisplay = true;
 
 function displayCountries(countries) {
 
     document.getElementById('container').innerText = '';
+
     for (let country of countries) {
         const capital = country.capital ? country.capital[0] : '-';
         const name = country.name.common;
         const flag = country.flags.svg;
         const population = country.population;
         const region = country.continents[0];
+        const id = country.cca2;
 
+        if (favourites.includes(id) && isFirstDisplay) {
+            addToList(flag, name, id);
+            favCountries.push(country);
+        }
         const card = `
-        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 border-0"> 
+        <div class="col ${countries.length === 2 && 'col-md-6'} ${countries.length !== 1 && 'col-md-4'} border-0 mb-5"> 
             <div class="card shadow-sm border-0" id="card">
-                <a href="./details.html?name=${name}">
-                    <img src="${flag}" alt="${name}" class="card-img-top">
+                <a href="./details.html?name=${name}" draggable="true" id="${id}"
+                 ondragstart={dragStart(event)} ondragend={dragEnd(event)}>
+                    <img src="${flag}" alt="${name}" class="card-img-top" id="${id}">
                     <div class="card-body bg-white toggle ${(darkMode) && "element-toggle "}">
                         <h5 class="card-title mb-3 mt-2 fw-bold">
                             ${name}
@@ -48,32 +57,35 @@ function displayCountries(countries) {
         `;
 
         document.getElementById('container').insertAdjacentHTML('beforeend', card);
-
-
     }
-}
 
+    isFirstDisplay = false;
+}
 
 
 search();
 
 function clearFilter() {
-    document.getElementById('filter').innerText = 'Filter by Region';
+    document.getElementById('filter').innerText = 'Filter by';
     document.getElementById('clear').style.display = 'none';
     search();
 }
 
-function filterByRegion(region) {
+function filter(filter) {
 
     document.getElementById('clear').style.display = 'block';
-    document.getElementById('filter').innerText = region;
+    document.getElementById('filter').innerText = filter;
 
-    const c = allCountries.filter(country => {
-        if (country.region.includes(region)) {
-            return country;
-        }
-    });
-
+    let c;
+    if (filter === 'Favourites') {
+        c = favCountries;
+    } else {
+        c = allCountries.filter(country => {
+            if (country.region.includes(filter)) {
+                return country;
+            }
+        });
+    }
     currCountries = c;
 
     displayCountries(c);
@@ -82,7 +94,7 @@ function filterByRegion(region) {
 
 async function search() {
     const search = document.getElementById('search').value;
-    const isFilterCleared = document.getElementById('filter').innerText === 'Filter by Region';
+    const isFilterCleared = document.getElementById('filter').innerText === 'Filter by';
 
     let res;
 
@@ -138,4 +150,75 @@ function toggleMode() {
 
 if (darkMode) {
     activateMode();
+}
+
+
+function dragStart(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+    event.target.style.opacity = '0.5';
+}
+
+function dragEnd(event) {
+    event.target.style.opacity = '1';
+}
+
+function dragEnter(event) {
+
+    if (event.target.className.includes("favourites"))
+        event.target.style.border = '1px solid #27ae60';
+}
+
+function dragLeave(event) {
+    event.target.style.border = 'none';
+}
+
+function drop(event) {
+    event.preventDefault();
+    if (event.target.className.includes("favourites")) {
+
+        const data = event.dataTransfer.getData("Text");
+        const node = document.getElementById(data).cloneNode(true);
+        node.style.opacity = '1';
+        event.target.style.border = 'none';
+
+        if (!favourites.includes(data)) {
+            const appendedNode = formatFavourite(node.childNodes[1].currentSrc, node.childNodes[3].childNodes[1].innerText, data);
+            favourites = [...favourites, data];
+            favCountries = [...favCountries, currCountries.find(e => e.cca2 === data)];
+            localStorage.setItem('fav', JSON.stringify(favourites));
+            event.target.insertAdjacentHTML('beforeend', appendedNode);
+        }
+    }
+}
+
+function formatFavourite(src, name, id) {
+    return `<div class="d-flex align-items-center justify-content-between mb-2"  id="${id}">
+                <div class="d-flex align-items-center">
+                     <img class="w-25 rounded m-0" src="${src}">
+                     <p class="m-0 ms-1 fw-bold" style="font-size: 0.6rem">
+                        ${name}
+                    </p>
+                </div>
+                <span class="rounded me-2 remove toggle ${(darkMode) && "element-toggle"}" onclick="removeCountry(event)" id="${id}">X</span>
+            </div>`;
+}
+
+function addToList(src, name, id) {
+
+    const appendedNode = formatFavourite(src, name, id);
+
+    document.getElementById('favourites').insertAdjacentHTML('beforeend', appendedNode);
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function removeCountry(event) {
+
+    const id = event.target.parentElement.id;
+    favourites = favourites.filter(e => e !== id);
+    localStorage.setItem('fav', JSON.stringify(favourites));
+    favCountries = favCountries.filter(c => c.cca2 !== id);
+    document.getElementById(id).remove();
 }
